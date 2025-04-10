a81b9fbb91817cbc0fb3fe860b775bc8028bfe97
Cargons <carlosgoncvls@gmail.com>
2025-04-10 12:12:55 +0100

    Admin

--- assistio/helpdesk_backend/tickets/admin.py
+++ assistio/helpdesk_backend/tickets/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Ticket, Client, Operator
 
-# Register your models here.
+@admin.register(Ticket)
+class TicketAdmin(admin.ModelAdmin):
+    list_display = ('title', 'status', 'created_at')  # Campos visíveis na listagem
+    list_filter = ('status', 'created_at')  # Filtros laterais
+    search_fields = ('title', 'description')  # Campos para barra de busca
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'birth_date')
+    search_fields = ('name', 'email')
+
+@admin.register(Operator)
+class OperatorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'birth_date')
+    search_fields = ('name', 'email')
